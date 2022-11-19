f0e3d443fd41fa2a5f567fb88317c6a260447d81
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2022-11-20 00:44:17 +0300

    4_2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* Page coupe */
+router.get('/coupe', function(req, res, next) {
+  res.send("<h1>Page coupe</h1>")
+});
+
+/* Page crossover */
+router.get('/crossover', function(req, res, next) {
+  res.send("<h1>Page crossover</h1>")
+});
+
+/* Page sedan */
+router.get('/sedan', function(req, res, next) {
+  res.send("<h1>Page sedan</h1>")
+});
 
 module.exports = router;
