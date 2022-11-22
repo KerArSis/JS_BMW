2bacebab1aa3d8d9a7df8385ad0373bf5b16c5c7
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2022-11-22 15:28:21 +0300

    7_2

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-var Car = mongoose.model('Car', { name: String })
+var schema = mongoose.Schema({ name: String })
 
-var boomer = new Car({ name: 'sedan' })
+schema.methods.vrruummm = function(){
+    console.log(this.get("name") + " сказал вррууммм")
+}
+
+var Car = mongoose.model('Car', schema)
+
+var boomer = new Car({ name: 'Sedan' })
 boomer.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('вррууммм')
-    }
+    boomer.vrruummm()
 })
