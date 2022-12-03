2d7918639f16b11b3b5e4ce34b345ef3b46c445e
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2022-12-03 15:08:07 +0300

    9_2

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Car = require("../models/car").Car
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+Car.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
+  res.cookie('greeting', 'Hi!!!').render('index', {
+      title: 'Express',
+      menu: menu
+  });
+})
+
+
 /* Page coupe */
 router.get('/coupe', function(req, res, next) {
   res.render('bmw', {
