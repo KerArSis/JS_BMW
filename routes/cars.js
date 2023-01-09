479123c6a132240192e372ab9c652fd413cba0e3
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-01-09 19:11:12 +0300

    11_3

--- routes/cars.js
+++ routes/cars.js
@@ -1,24 +1,49 @@
 var express = require('express');
 var router = express.Router();
-var Car = require("../models/car").Car
+var db = require('../mySQLConnect'); 
+//  var Car = require("../models/car").Car;
+//  var checkAuth = require("./../middleware/checkAuth.js")
+var async = require("async")
 
-/* GET users listing. */
+
+
+
+/* GET cars listing. */
 router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с cars');
+  res.send('Новый маршрутизатор, для маршрутов, начинающихся с cars')
 });
 
-/* Page cars */
-router.get('/:nick', function(req, res, next) {
-    Car.findOne({nick:req.params.nick}, function(err,car){
-        if(err) return next(err)
-        if(!car) return next(new Error("Нет такого автомобиля"))
-        res.render('car', {
-            title: car.title,
-            picture: car.avatar,
-            desc: car.desc
-        })
-    })
+
+/* Страница машин */
+router.get('/:nick' ,function(req, res, next) {
+  db.query(`SELECT * FROM cars WHERE cars.nick = '${req.params.nick}'`, (err, cars) => {
+    if(err) {
+      console.log(err);
+      if(err) return next(err)
+    } else {
+      if(cars.length == 0) return next(new Error("Нет такой bmw"))
+      var car = cars[0];
+      res.render('car', {
+        title: car.title,
+        picture: car.avatar,
+        desc: car.about
+      })
+    }
+  })
 })
 
 
+/* Страница машин на mongodb*/
+// router.get('/:nick', checkAuth,function(req, res, next) {
+//  Car.findOne({nick:req.params.nick}, function(err,Car){ 
+//      if(err) return next(err)
+//      if(!Car) return next(new Error("Нет такой bmw"))
+//      res.render('Car', {
+//          title: Car.title,
+//          picture: Car.avatar,
+//          desc: Car.desc
+//      })
+// })
+// })
+// .
 module.exports = router;
