f56e836f9628a9b852e52489fff0844e7e167d0f
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-01-10 16:43:12 +0300

    11_6

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,47 @@
-var express = require('express');
-var router = express.Router();
-var Car = require("../models/car").Car
+var express = require('express')
+var router = express.Router()
+var db = require("../mySQLConnect.js");
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  Car.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
-      req.session.greeting = "Hi!!!",
-          res.cookie('greeting', 'Hi!!!').render('index', {
-              title: 'Express',
-              menu: menu
-          });
-  })
+
+
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Проект "BMW"', counter:req.session.counter  });
 });
 
-/* Page coupe */
-router.get('/coupe', function(req, res, next) {
-  res.render('bmw', {
-    title: "Coupe",
-    picture: "images/coupe.jpg",
-    desc: "BMW 8 серии — это роскошное купе, второе поколение которого выпускается в Германии с 2018 года. Автомобили для России оснащаются шестицилиндровым турбодизелем объёмом три литра и турбомотором V8 4.4. У всех машин — полный привод и восьмиступенчатая автоматическая коробка передач."
-  })
+
+router.get('/logreg', function(req, res, next) {
+res.render('logreg',{title: 'Вход', error: null});
 });
-/* Page crossover */
-router.get('/crossover', function(req, res, next) {
-  res.render('bmw', {
-    title: "Crossover",
-    picture: "images/crossover.jpg",
-    desc: "BMW X5 — это кроссовер, четвертое поколение которого выпускается с 2018 года в США под заводским индексом G05. В России автомобиль предлагается с бензиновыми и дизельными турбомоторами. У всех версий — полный привод и восьмиступенчатый «автомат», а в салоне может быть пять или семь мест."
-  })
+
+
+
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  // console.log("hey")
+  db.query (`SELECT * FROM user WHERE user.username = '${req.body.username}'`, function(err,users){
+        if(err) return next(err)
+        if(users.length > 0) {
+          var user = users[0];
+          if (password == user.password){
+            req.session.user = user.user_id
+            res.redirect('/')
+          } else {
+            res.render('logreg', {title: 'Вход', error: 'Пароль не верный'})
+          }
+        } else {
+          db.query(`INSERT INTO user (username, password) VALUES ('${username}', '${password}')`, function(err, user){
+            if(err) return next(err)
+            req.session.user = user.user_id
+            res.redirect('/')
+          })
+        }
+})
 });
-/* Page sedan */
-router.get('/sedan', function(req, res, next) {
-  res.render('bmw', {
-    title: "Sedan",
-    picture: "images/sedan.jpg",
-    desc: "BMW E60 — это пятое поколение седанов 5 серии, пришедшее на смену E39 и выпускалось с 2003 по 2010 год. Внешность кузова был разработан дизайнером Davide Arcangeli, и следует отметить, что для своей нетрадиционной внешности, и несмотря на некоторую критику, седан стал самой продаваемой маркой БМВ в своем классе."
-  })
+router.post('/logout', function(req, res, next) {
+  req.session.destroy()
+  req.session.user = null
+  res.redirect('/')
 });
 module.exports = router;
